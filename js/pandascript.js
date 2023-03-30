83fff258a381ce0e66b50d6725b363cec823ba2c
Mr. Mac-Flo (The Assist) <70183048+maciam21@users.noreply.github.com>
2023-03-29 19:23:40 -0700

    added files

---
+++ js/pandascript.js
@@ -0,0 +1,32 @@
+function addTask() {
+    let input = document.getElementById("taskInput");
+    let task = input.value;
+    let ul = document.getElementById("taskList");
+    let li = document.createElement("li");
+    let checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.onclick = function() {
+        li.classList.toggle("completed");
+    };
+    li.appendChild(checkbox);
+    li.appendChild(document.createTextNode(task));
+    ul.appendChild(li);
+    input.value = "";
+}
+
+function completeTasks() {
+    let lis = document.getElementsByTagName("li");
+    for (let i = 0; i < lis.length; i++) {
+        if (!lis[i].classList.contains("completed")) {
+            lis[i].classList.add("completed");
+            lis[i].getElementsByTagName("input")[0].checked = true;
+        }
+    }
+}
+
+function clearTasks() {
+    let ul = document.getElementById("taskList");
+    while (ul.firstChild) {
+        ul.removeChild(ul.firstChild);
+    }
+}
